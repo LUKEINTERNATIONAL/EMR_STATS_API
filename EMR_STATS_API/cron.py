42cda26e790adf19212720f3104ce82be3add8eb
424275 <hallo1144@gmail.com>
2022-11-04 11:53:37 +0200

    add datetime

--- EMR_STATS_API/cron.py
+++ EMR_STATS_API/cron.py
@@ -1,10 +1,14 @@
 from encounters.views import EncouterDetails
+from datetime import datetime
+
 def my_scheduled_job():
     encounters = EncouterDetails()
     print("**************************************")
     print("Start processing data")
+    print(datetime.now())
     print("**************************************")
     encounters.process_all_facilities()
     print("**************************************")
     print("End processing data")
+    print(datetime.now())
     print("**************************************")
