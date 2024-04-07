98712b6c2d797e63afbab647387ff05b3f84517b
petroskayange <kayangepetros@gmail.com>
2024-04-07 11:59:42 +0200

    improving failing to enter in some server

--- databases/tasks.py
+++ databases/tasks.py
@@ -20,7 +20,7 @@ def copy_emr_dumps(facility):
         print("Start copying from "+facility['facility_name'])
         facility_name = facility['facility_name'].replace(' ', '_')
         make_dir("~/Facilities_Backups/"+facility_name)
-        os.system("sshpass -p '{}' rsync -vP -r -e 'ssh -o StrictHostKeyChecking=no -p 22' {}@{}:~/remote_backups ~/Facilities_Backups/{}"
+        os.system("sshpass -p '{}' rsync -vP -r -e 'ssh -o StrictHostKeyChecking=no -o VerifyHostKeyDNS=no -o UserKnownHostsFile=/dev/null -p 22' {}@{}:~/remote_backups ~/Facilities_Backups/{}"
         .format(facility['password'],facility['user_name'],facility['ip_address'],facility_name))
     except:
         print("Error can not copy emr dump from "+facility['facility_name'])
@@ -30,7 +30,7 @@ def copy_iblis_dumps(facility):
         print("Start copying from "+facility['facility_name'])
         facility_name = facility['facility_name'].replace(' ', '_')
         make_dir("~/Facilities_Backups/"+facility_name)
-        os.system("sshpass -p '{}' rsync -vP -r -e 'ssh -o StrictHostKeyChecking=no -p 22' {}@{}:~/remote_backups ~/Facilities_Backups/{}"
+        os.system("sshpass -p '{}' rsync -vP -r -e 'ssh -o StrictHostKeyChecking=no -o VerifyHostKeyDNS=no -o UserKnownHostsFile=/dev/null -p 22' {}@{}:~/remote_backups ~/Facilities_Backups/{}"
         .format(facility['password_iblis'],facility['user_name_iblis'],facility['ip_address_iblis'],facility_name))
     except:
         print("Error can not copy iblis dump from "+facility['facility_name'])
@@ -49,10 +49,10 @@ def create_emr_dumps(facility):
         facility_name = facility['facility_name'].replace(' ', '_')
         make_dir("~/Facilities_Backups/"+facility_name)
         
-        rsync_command = "sshpass -p '{}' rsync -vP -r -e 'ssh -o StrictHostKeyChecking=no -p 22' /var/www/EMR_STATS_API/bin/emr_remote_auto_database_backup.sh {}@{}:~/ ".format(facility['password'], facility['user_name'], facility['ip_address'])
+        rsync_command = "sshpass -p '{}' rsync -vP -r -e 'ssh -o StrictHostKeyChecking=no -o VerifyHostKeyDNS=no -o UserKnownHostsFile=/dev/null -p 22' /var/www/EMR_STATS_API/bin/emr_remote_auto_database_backup.sh {}@{}:~/ ".format(facility['password'], facility['user_name'], facility['ip_address'])
         os.system(rsync_command)
         
-        ssh_command = "sshpass -p {} ssh -o StrictHostKeyChecking=no {}@{} 'bash -s' < {}  >> ~/Facilities_Backups/all_dumps.log 2>&1 &".format(facility['password'], facility['user_name'], facility['ip_address'], '/var/www/EMR_STATS_API/bin/emr_remote_auto_database_backup.sh')
+        ssh_command = "sshpass -p {} ssh -o StrictHostKeyChecking=no -o VerifyHostKeyDNS=no -o UserKnownHostsFile=/dev/null -p 22 {}@{} 'bash -s' < {}  >> ~/Facilities_Backups/all_dumps.log 2>&1 &".format(facility['password'], facility['user_name'], facility['ip_address'], '/var/www/EMR_STATS_API/bin/emr_remote_auto_database_backup.sh')
         subprocess.run(ssh_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     except Exception as e:
         print("Error can not create emr backup for {}: {}".format(facility['facility_name'], str(e)))
@@ -63,10 +63,10 @@ def create_iblis_dumps(facility):
         facility_name = facility['facility_name'].replace(' ', '_')
         make_dir("~/Facilities_Backups/"+facility_name)
         
-        rsync_command = "sshpass -p '{}' rsync -vP -r -e 'ssh -o StrictHostKeyChecking=no -p 22' /var/www/EMR_STATS_API/bin/iblis_remote_auto_database_backup.sh {}@{}:~/ ".format(facility['password_iblis'], facility['user_name_iblis'], facility['ip_address_iblis'])
+        rsync_command = "sshpass -p '{}' rsync -vP -r -e 'ssh -o StrictHostKeyChecking=no -o VerifyHostKeyDNS=no -o UserKnownHostsFile=/dev/null -p 22' /var/www/EMR_STATS_API/bin/iblis_remote_auto_database_backup.sh {}@{}:~/ ".format(facility['password_iblis'], facility['user_name_iblis'], facility['ip_address_iblis'])
         os.system(rsync_command)
         
-        ssh_command = "sshpass -p {} ssh -o StrictHostKeyChecking=no {}@{} 'bash -s' < {}  >> ~/Facilities_Backups/all_dumps.log 2>&1 &".format(facility['password_iblis'], facility['user_name_iblis'], facility['ip_address_iblis'], '/var/www/EMR_STATS_API/bin/iblis_remote_auto_database_backup.sh')
+        ssh_command = "sshpass -p {} ssh -o StrictHostKeyChecking=no -o VerifyHostKeyDNS=no -o UserKnownHostsFile=/dev/null -p 22 {}@{} 'bash -s' < {}  >> ~/Facilities_Backups/all_dumps.log 2>&1 &".format(facility['password_iblis'], facility['user_name_iblis'], facility['ip_address_iblis'], '/var/www/EMR_STATS_API/bin/iblis_remote_auto_database_backup.sh')
         subprocess.run(ssh_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     except Exception as e:
         print("Error can not create iblis backup for {}: {}".format(facility['facility_name'], str(e)))
